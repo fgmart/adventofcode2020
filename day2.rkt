9a6c60c7cf69abf1e1b93f0d42a89dbe5fa60586
Fred Martin <fred_martin@uml.edu>
2020-12-04 23:00:12 -0500

    Add files via upload

---
+++ day2.rkt
@@ -0,0 +1,67 @@
+#lang racket
+
+; Fred Martin fredm@alum.mit.edu
+; Day 2 of Advent of Code 2020
+; Dec 5 2020
+; part one answer so far
+
+(define in (open-input-file "day2-input.txt"))
+
+; file looks like...
+
+;4-8 g: ggtxgtgbg
+;13-14 q: qmzdrtqctvrqsb
+;1-5 d: ddddlddzfdd
+;1-3 f: pfhff
+;4-5 c: ccssnccccc
+;etc.
+
+; to get answer:
+; (read-and-process 0)
+
+; reads a line of the file and hands it off to be processed,
+; recursing to count how many lines pass the test
+(define (read-and-process count)
+  (let ((str (read-line in)))
+    (if (eof-object? str) count
+        (if (process-line str) (read-and-process (+ 1 count))
+            (read-and-process count)))))
+
+; parses a line into four parts and returns them as a list:
+; (min-count max-count char pwd)
+(define (parse-line str)
+  (let* ((split (string-split str " "))
+         (num-range-str (first split))
+         (num-range (map string->number (string-split num-range-str "-")))
+         (min (first num-range))
+         (max (second num-range))
+         (char (string-ref (second split) 0))
+         (pwd (third split)))
+    (list min max char pwd)))
+
+; takes the list-of-four above and
+; counts the num of matching chars in the pwd
+; and says #t or #f depending whether it fits the min/max range
+(define (process-line str)
+  (let* ((quad (parse-line str))
+         (min (first quad))
+         (max (second quad))
+         (char (third quad))
+         (pwd (fourth quad))
+         (numchars (count-chars pwd char)))
+    (and (>= numchars min) (<= numchars max))))
+    
+
+; surely there must be a better way?
+; counts occurrances of char in str by
+; converting str to list of chars;
+; mapping eq fcn to change those chars into 1s and 0s
+; adding up the 1s.
+(define (count-chars str char)
+  (let* ((char-list (string->list str))
+         (match (lambda (c) (if (eq? c char) 1 0)))
+         (char-matches (map match char-list)))
+    (foldr + 0 char-matches)))
+
+         
+             
